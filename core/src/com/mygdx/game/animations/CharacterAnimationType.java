b7ee5bd2cc2b0ab94a903c9888c5643d834f2394
Aidan K <34078276+aidankay1@users.noreply.github.com>
2022-12-28 13:42:14 -0500

    move animation classes into their own package

--- core/src/com/mygdx/game/animations/CharacterAnimationType.java
+++ core/src/com/mygdx/game/animations/CharacterAnimationType.java
@@ -1,4 +1,4 @@
-package com.mygdx.game;
+package com.mygdx.game.animations;
 
 public enum CharacterAnimationType {
   MAGEIDLE,
--- core/src/com/mygdx/game/animations/CharacterAnimationTypeException.java
+++ core/src/com/mygdx/game/animations/CharacterAnimationTypeException.java
@@ -1,4 +1,4 @@
-package com.mygdx.game;
+package com.mygdx.game.animations;
 
 public class CharacterAnimationTypeException extends RuntimeException {
 
